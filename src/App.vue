c20ce9b310ce84fcf363e61e62a0477afe16deef
Samk13 <samarbid13@gmail.com>
2020-07-16 00:05:51 +0200

    make positive chart working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container box-border m-auto w-full bg-gray-200">
+    <div class="p-5 text-5xl font-black text-center text-red-800">
+      covid-19 tracking app
+    </div>
+    <div class="mt-5" v-if="positiveCases.length > 0">
+      <h2 class="text-xl font-black">Positive</h2>
+      <LineCharts 
+        :chartData="positiveCases"
+        :options="chartOptions"
+        :label="positiveCases"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import moment from 'moment'
+import LineCharts from './components/LineCharts'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LineCharts
+  },
+  data(){
+    return {
+      positiveCases: [],
+      hospitalization: [],
+      inIcu: [],
+      onVentilators:[],
+      recovered: [],
+      deaths: [],
+      chartOptions: {
+        responsive: true,
+        maintainAscpectRatio: false
+      }
+    }
+  },
+  async created(){
+    const { data } = await axios.get('https://covidtracking.com/api/us/daily')
+    // console.log(data)
+    data.forEach(day => {
+      const date = moment(day.date, 'YYYYMMDD').format('MM/DD')
+      const {
+        positive,
+        hospitalizedCurrently,
+        inIcuCurrently,
+        onVentilatorCurrently,
+        recovered,
+        death
+      } = day
+      this.positiveCases.push({date, total: positive})
+      this.hospitalization.push({ date, total: hospitalizedCurrently})
+      this.inIcu.push({date, total: inIcuCurrently})
+      this.onVentilators.push({date, total: onVentilatorCurrently})
+      this.recovered.push({date, total: recovered})
+      this.deaths.push({date, total: death})
+    })
+    console.log();
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/LineCharts.vue
@@ -0,0 +1,35 @@
+// No need for template tag for vue-charts, it's just JS
+<script>
+import { Line } from "vue-chartjs";
+export default {
+  extends: Line,
+  props: {
+    labels: {
+      type: Array,
+    },
+    chartData: {
+      type: Array,
+    },
+    options: {
+      type: Object,
+    },
+  },
+  mounted() {
+    const dates = this.chartData.map((d) => d.date).reverse()
+    const totals = this.chartData.map((d) => d.total).reverse()
+
+    this.renderChart(
+      {
+        labels: dates,
+        datasets: [
+          {
+            label: this.label,
+            data: totals,
+          },
+        ],
+      },
+      this.options
+    );
+  },
+};
+</script>
